4e573b1d44a4daa91343c9b8159bd967783fce3b
Mohsen Azimi <me@azimi.me>
2015-07-19 13:37:27 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# YAML Worker
+
+> JS-YAML and YAML-JS wrapper with Worker API
+
+#### Installation
+
+Install using Bower or npm
+
+```
+npm install --save yaml-worker
+```
+
+
+```
+bower install --save yaml-worker
+```
+
+#### Usage
+
+When calling `postMessage`, pass an array which first element in array is the method name and the rest are arguments for that method.
+
+```js
+var YAMLWorker = new WebWorker('/path/to/yaml-worker.js');
+
+YAMLWorker.postMessage(['load', 'value: 1']);
+
+YAMLWorker.onmessage = function(message) {
+  console.log('Result is ', message.data);
+};
+```
+
+### License
+MIT
---
+++ index.js
@@ -0,0 +1,32 @@
+var YAMLJS = require('yaml-js');
+var JSYAML = require('js-yaml');
+
+/**
+ * Worker message listener.
+ *
+ * @param  {object} message Web Workr message object
+ *
+ * # Message format:
+ * `message` is an array. first argument in the array is the method name string
+ * and the rest of items are arguments to that method
+ */
+this.onmessage = function onmessage(message) {
+
+  if (!Array.isArray(message.data) || message.data.length < 2) {
+    throw new TypeError('data should be an array with method and arguments');
+  }
+
+  var method = message.data[0];
+  var args =message.data.slice(1);
+  var result = null;
+
+  if (typeof JSYAML[method] === 'function') {
+    result = JSYAML[method].apply(args);
+  } else if (typeof YAMLJS[method] === 'function') {
+    result = YAMLJS[method].apply(args);
+  } else {
+    throw new TypeError('bad method name');
+  }
+
+  postMessage(JSON.stringify(result));
+};
