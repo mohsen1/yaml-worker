d0611d4c731bc51e9d315ccb3f472e3d0ff5b004
Mohsen Azimi <me@azimi.me>
2015-07-20 10:15:32 -0700

    Fix tests

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'spec/tests.js',
-      {pattern: '/browser.js', included: false, served: true}
+      'browser.js',
+      'spec/tests.js'
     ],
 
 
--- spec/tests.js
+++ spec/tests.js
@@ -1,10 +1,26 @@
+'use strict';
+
 describe('YAML Wroker', function () {
-  var YAMLWoker = new Worker('../browser.js');
+
+  var YAMLWoker;
+
+  it('creates the worker', function() {
+    var error;
+
+    try {
+      YAMLWoker = new Worker('base/browser.js');
+    } catch(err) {
+      error = err;
+    }
+
+    expect(error).to.be.undefined;
+  })
 
   it('#load', function (done) {
     YAMLWoker.postMessage(['load', 'val: 1']);
 
     YAMLWoker.onmessage = function (message) {
+      expect(message.data).to.deepEqual({val: 1});
       done();
     };
   });
