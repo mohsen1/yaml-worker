e05c54676b87c501c0c55953f8e8c13522be99dc
Mohsen Azimi <me@azimi.me>
2015-09-08 23:17:30 -0700

    Select browser from environment

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
+    browsers: [process.env.browser || 'Chrome'],
 
 
     // Continuous Integration mode
